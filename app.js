1d5d0456d3f7d2d88052e75bbe6075512e28a30f
Ramil <cryfrost0@gmail.com>
2024-10-18 12:01:12 +0400

    Переменные и арифмитические операторы

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const width = 10;
+const height = 5;
+const space = width * height;
+
+let a = 56, b = 67;
+
+let c = a ** 3; //a*a*a
+
+console.log(c);
+
+const city = 'Москва';
+
+const street = 'Новослободская';
+
+console.log(city + ', ' + street + ' ' + 5);
+
+
+
+
+
+
