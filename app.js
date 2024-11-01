7e72efd2cbeb9f0e0865e012f264de532688c48c
Ramil_laptop <ramil.uraznazarov@yandex.ru>
2024-11-01 21:37:12 +0400

    задача со строками

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const userName = 'Вася aka terminator perdinator Пупкин';
+const num1 = "89103235356";
+const num2 = "+79103235356";
+const num3 = "+7(910)3235356";
+const num4 = "+7(910) 323-53-56";
+const num5 = "   +7(910) 323-53-56";
+const num1Error = "89103235";
+const num2Error = "+7d910d323-53-56";
+const num3Error = "9+7103235356";
+const num4Error = "89103g35356";
+let fixedNum = "";
 
-let name = userName.slice(0, 4);
-let lastName = userName.slice(31);
+function checkN(num) {
+  fixedNum = num.trim();
+  //console.log(fixedNum);
 
-let name1 = userName.indexOf('Вася');
+  console.log(fixedNum.lenght);
 
-console.log(name);
-console.log(lastName);
-console.log(name1);
+  if (num.lenght < 10) {
+    return false;
+  } else {
+    return fixedNum;
+  }
+}
+
+console.log(checkN(num1Error));
